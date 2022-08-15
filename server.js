498514522d6f91032298b6ef650ff5a3197c6440
DSK9012 <saikiran.dannana@gmail.com>
2022-08-15 19:06:54 +0530

    created a prod build and tried to deploy, but need to learn a lot yet

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const gqlSchema = require('./graphql/schema');
 const { movie, searchedMovies } = require('./graphql/resolvers');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
 
@@ -18,5 +19,18 @@ app.use(
     graphiql: true,
   })
 );
+app.use(express.static(path.join(__dirname, './client/out')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/out/'));
+});
 
 app.listen(5000, () => console.log('server listening on port 5000'));
+
+// Production Mode
+// if (process.env.NODE_ENV.toLowerCase() === 'production') {
+//   // Serve Any Static Files
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+//   app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//   });
+// }
